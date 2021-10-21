43db12f2904f3fdcd3fcdcb1e3649b9fe562f1ed
pAndressa <p.andressasilva@gmail.com>
2021-10-20 21:38:40 -0300

    Adicionando propriedades

--- AppDomain/Entities/Receita.cs
+++ AppDomain/Entities/Receita.cs
@@ -2,6 +2,25 @@ namespace AppDomain.Entities
 {
     public class Receita
     {
-        
+        public int ReceitaId { get; private set; }
+        public string Nome { get; private set; }
+        public string ModoPreparo { get; private set; }
+        public DateTime TempoPreparo { get; private set; }
+        public int Rendimento { get; private set; }
+
+        public void CadastrarReceita()
+        {
+
+        }
+
+        public void AlterarReceita()
+        {
+
+        }
+
+        public void AlterarStatus()
+        {
+            
+        }
     }
 }
